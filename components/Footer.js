153722db5131020344889475e0e70e4eccc107ef
pengchongfu <pengchongfu@gmail.com>
2017-06-29 16:45:09 +0800

    component Footer

---
+++ components/Footer.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Component from 'nuxt-class-component'
+
+import styles from '~assets/footer.css'
+
+@Component
+export default class Footer extends Vue {
+  render () {
+    return (
+      <div class={ styles.footer }>
+        <div class="container">
+          <div class={ styles.copyright }>
+            ©2017 Ye HongXing
+          </div>
+          <div class={ styles.icons }>
+            <i class={ `fa fa-facebook ${styles.icon}` } aria-hidden="true"></i>
+            <i class={ `fa fa-twitter ${styles.icon}` } aria-hidden="true"></i>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
