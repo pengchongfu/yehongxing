c70b782bc2533a75bacf6bc7c3cff4e6ccb0bbc7
pengchongfu <pengchongfu@gmail.com>
2017-06-30 11:41:05 +0800

    modify layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,16 @@
 <script>
 import Header from '~components/Header'
+import Footer from '~components/Footer'
+
+import styles from '~assets/default.css'
 
 export default {
   render () {
     return (
       <div>
         <Header/>
-        <nuxt/>
+        <nuxt class={ styles.default }/>
+        <Footer/>
       </div>
     )
   }
