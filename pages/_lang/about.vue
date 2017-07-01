ed1cb561494e88a1b207e8fb856fc217d65da617
pengchongfu <pengchongfu@gmail.com>
2017-07-01 13:18:58 +0800

    some pages

---
+++ pages/_lang/about.vue
@@ -0,0 +1,26 @@
+<script>
+import Vue from 'vue'
+import Component from 'nuxt-class-component'
+
+import styles from '~assets/about.css'
+
+@Component
+export default class About extends Vue {
+  render () {
+    return (
+      <div class={ styles.about }>
+        <div class="container">
+          <div class="row">
+            <div class="col-4">
+              4
+            </div>
+            <div class="col-8">
+              8
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+</script>
---
+++ pages/_lang/work.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue'
+import Component from 'nuxt-class-component'
+
+import styles from '~assets/work.css'
+
+@Component
+export default class Work extends Vue {
+  render () {
+    return (
+      <div class={ styles.work }>
+        work
+      </div>
+    )
+  }
+}
+</script>
